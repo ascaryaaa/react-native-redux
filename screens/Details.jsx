b0dc2e218c1cc1b03f4c399e35e11bcde2ff3771
ascaryaaa <allaamascarya@gmail.com>
2024-03-01 09:07:39 +0700

    implement the counter to different screen

--- screens/Details.jsx
+++ screens/Details.jsx
@@ -1,9 +1,22 @@
-import { View } from "react-native"
+import { View, StyleSheet, Text } from "react-native"
+import { useDispatch, useSelector } from "react-redux"
+
 
 const detail = () => {
- return (
-    <View></View>
- )
+    const count = useSelector((state) => state.counter.count)
+    return (
+        <View style={styles.container}>
+            <Text>{count}</Text>
+        </View>
+    )
 }
 
 export default detail;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
